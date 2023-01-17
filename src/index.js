14841f2d0a3d5d50f59acf7d3f8b991867b033b5
Daniel Ramirez <funkadelick485@gmail.com>
2023-01-17 16:06:33 -0600

    Rick and morty store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-// console.log("Happy hacking :)");
+const urlBase = "https://rickandmortyapi.com/api/";
 
-const urlBase = "https://platzi-avo.vercel.app";
-const url = "https://platzi-avo.vercel.app/api/avo";
-
+/*
 //intl  --> formato a fechas o monedas
 const formatPrice = (precio) => {
   const newPrice = new window.Intl.NumberFormat("en-EN", {
@@ -17,43 +15,58 @@ const formatPrice = (precio) => {
   return newPrice;
 };
 
+// precio.textContent = formatPrice(element.price);
+*/
+
 const appNode = document.querySelector("#app");
 // web api
 // conectarnos al server
 window
-  .fetch(`${urlBase}/api/avo`) //devuelve promesa
+  .fetch(`${urlBase}character`) //devuelve promesa
   //procesar la respuesta y convertirla en JSON
   .then((respuesta) => respuesta.json())
   // JSON -> Data -> Rendeerizar infor browser
   .then((responseJson) => {
     const items = [];
-    responseJson.data.forEach((element) => {
+    responseJson.results.forEach((element) => {
       //create image
-      const imagen = document.createElement("img");
-      imagen.className =
-        "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
-      imagen.src = `${urlBase}${element.image}`;
+      const image = document.createElement("img");
+      image.className =
+        "h-16 w-16 md:h-24 md:w-24 rounded-lg mx-auto md:mx-0 md:mr-6";
+      image.src = element.image;
 
       //create title
-      const titulo = document.createElement("h2");
-      titulo.className = "text-lg";
-      titulo.textContent = element.name;
+      const title = document.createElement("h2");
+      title.className = "text-lg font-bold text-white";
+      title.textContent = element.name;
       // titulo.style = "font-size: 2rem";  titulo.style.fontSize = "2rem";
 
-      //create price
-      const precio = document.createElement("div");
-      precio.className = "text-gray-600";
-      precio.textContent = formatPrice(element.price);
+      //create status
+      const status = document.createElement("p");
+      status.className = "text-yellow-200";
+      status.textContent = element.status;
 
-      //contenedor para precio y titulo
-      const precioTitulo = document.createElement("div");
-      precioTitulo.className = "text-center md:text-left";
-      precioTitulo.append(titulo, precio);
+      //create species
+      const species = document.createElement("p");
+      species.className = "text-pink-400";
+      species.textContent = element.species;
 
-      //contenedor
+      //create location
+      const location = document.createElement("p");
+      location.className = "text-green-200";
+      location.textContent = element.location.name;
+
+      //contenedor texto
+      const titleStatus = document.createElement("div");
+      titleStatus.className = "text-center md:text-left";
+      titleStatus.append(title, status, species, location);
+
+      //contenedor general
       const card = document.createElement("div");
-      card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
-      card.append(imagen, precioTitulo);
+      card.className =
+        "md:flex rounded-lg p-6 m-2 bg-gray-700 hover:bg-gray-400";
+      // card.className = "bg-gray-700 gallery-card rounded-lg flex justify-around items-center p-6 hover:bg-gray-400";
+      card.append(image, titleStatus);
 
       items.push(card);
     });
